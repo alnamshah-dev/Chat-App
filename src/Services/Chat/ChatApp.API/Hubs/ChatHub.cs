8d0b3730fb225c382adf06510571d21f98332063
AbdulazizALNamshah <abdula2i2namsha@gmail.com>
2025-08-28 01:24:53 +0300

    Adding the models and hubs for Chat.API

---
+++ src/Services/Chat/ChatApp.API/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using ChatApp.API.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.API.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task JoinChat(UserConnection conn)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "admin", $"{conn.UserName} has joined");
+        }
+        public async Task JoinSpecificChatRoom(UserConnection conn)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
+            await Clients.Groups(conn.ChatRoom)
+                .SendAsync("ReceiveMessage", "admin", $"{conn.UserName} has joined {conn.ChatRoom}.");
+        }
+
+    }
+}
